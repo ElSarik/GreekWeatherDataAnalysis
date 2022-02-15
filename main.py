8ac82b1755dda6d6a2d6b2e06cd151623e416ab6
Elias Sarikakis <sarikakisilias@gmail.com>
2022-02-15 20:15:00 +0200

    Global data test

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from functions.dataset import dataset_open, dataset_split
+from functions.dataset import dataset_open, dataset_split, testfunc
 from functions.display import display_row_with_titles
 
 dataset = dataset_open()
 data = dataset_split(dataset)
 
 ###### Optional alternative print using 2nd argument (shows empty fields).
-indexes = [92, 93]
-display_row_with_titles(data, indexes)
+# indexes = [92, 93]
+# display_row_with_titles(data, indexes)
 
 ###### Prints with first line of data - without 2nd argument.
 # display_row_with_titles(data)
+
+
+testfunc()
