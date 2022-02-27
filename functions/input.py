3ba2dcadf0624f512955b9c07176d17956b1d9f8
ElSarik <sarikakisilias@gmail.com>
2022-02-27 12:14:07 +0200

    Created input.py file in functions
    - Added a new file called input.py inside the functions folder, meant to host all functions related to string manipulations, error checking, etc.
    - Added an input capitalization function which takes a string input and returns its capitalization form.

---
+++ functions/input.py
@@ -0,0 +1,8 @@
+def input_capitalization(input: str):
+    '''Capitalizes the input and returns the result,
+    
+       arg: input of type string,
+       
+       returns the capitalized input as string.'''
+
+    return input.capitalize()
