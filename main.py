28b69ba6025747d3343e95eaec1f8b13c15f4296
ElSarik <sarikakisilias@gmail.com>
2022-03-19 14:32:13 +0200

    Rearranged task execution

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ data = dataset_split(dataset)
 
 
 
-get_highest_temperature_by_year(2008, 2010)              #task 3
+print(five_highestorlowest_temperature_cities_in2018())  #task 3
 print("=====================")
-get_highest_temperature_by_year()                        #task 3
+print(middleterm_of_place_in_2006_and_2018('Μακεδονία')) #task 4
 print("=====================")
-get_highest_amount_of_rain()                             #task 4
+print(middleterm_of_place_in_2006_and_2018("Ν. Αιγαίου"))#task 4
 print("=====================")
-print(five_highestorlowest_temperature_cities_in2018())  #task 5
+get_highest_temperature_by_year(2008, 2010)              #task 5
 print("=====================")
-print(middleterm_of_place_in_2006_and_2018('Μακεδονία')) #task 6
+get_highest_temperature_by_year()                        #task 5
 print("=====================")
-print(middleterm_of_place_in_2006_and_2018("Ν. Αιγαίου"))
+get_highest_amount_of_rain()                             #task 6
